99093d5b70d5020aa0fed111f953274e0a0af87a
BuildTools <arthurpisakhov1@gmail.com>
2019-08-19 22:34:03 -0400

    instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,20 @@ class Person {
     return `Hello my name is ${this.name} and I am from ${this.location}.`
   }
 }
+
+class Instructor extends Person {
+  constructor(instAttrs) {
+    super(instAttrs)
+    this.specialty = instAttrs.specialty;
+    this.favLanguage = instAttrs.favLanguage;
+    this.catchPhrase = instAttrs.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}.`
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}.`
+  }
+}
